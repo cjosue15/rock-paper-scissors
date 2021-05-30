1fd1c90df49602ce3176123df3a4737a8c421915
Carlos Morales <carlosjosuemn98@gmail.com>
2021-05-29 22:33:20 -0500

    importaciones de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { ItemComponent } from './componentes/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
