0eacd7e0d1ba0e56d6989130764c98386c9f413e
Carlos Morales <carlosjosuemn98@gmail.com>
2021-06-11 16:56:40 -0500

    home finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayService } from '../../core/services/play.service';
+import { TypeItem } from '../../models/type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  title: string | null;
+  constructor(private playService: PlayService) {
+    this.title = '';
   }
 
+  ngOnInit(): void {
+    this.playService.playObservable.subscribe(
+      (response: string | null) => (this.title = response)
+    );
+  }
+
+  chooseItem(event: TypeItem): void {
+    this.playService.changePlayObservable = event;
+  }
 }
